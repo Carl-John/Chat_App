53c2b16cf1316c01ebade03fac1f91cd836f2239
Carl_John <jy081320@gmail.com>
2022-12-10 19:22:07 -0800

    Socket io Events

--- src/index.js
+++ src/index.js
@@ -12,18 +12,17 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
+
 
 io.on('connection', (socket) => {
     console.log('New Web Socket connection')
 
-    socket.emit('countUpdated', count)
+    socket.emit('message', 'Welcome!')
 
-    socket.on('increment', () => {
-        count++
-        // socket.emit('countUpdated', count)
-        io.emit('countUpdated', count)
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
     })
+
 })
 
 server.listen(port, () => {
